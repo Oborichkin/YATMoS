ef6b98256f2037f3195948f021ea5381ce26bdfd
Pavel Oborin <oborin.p@gmail.com>
2022-12-19 02:35:03 +0300

    🎨 Adding simple frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>YATMoS</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>Dashboard</v-btn>
+      <v-btn text>Projects</v-btn>
+      <v-btn text>Runs</v-btn>
+    </v-app-bar>
+
+    <v-main> </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
